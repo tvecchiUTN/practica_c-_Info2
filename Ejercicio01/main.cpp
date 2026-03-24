684e1c08a6a553c3dbb66786f51b6f90ed2a4193
Tomas Vecchi <204607402+tvecchiUTN@users.noreply.github.com>
2026-03-24 20:51:30 -0300

    Ejercicio 01 completado
    Este incluye:
    Solicitar valores por consola
    Dar valores por referecia
    Uso de memoria dinamica

--- Ejercicio01/main.cpp
+++ Ejercicio01/main.cpp
@@ -6,18 +6,49 @@ using namespace std;
     Ejercicio 1: Herramientas de nuestro nuevo entorno de desarrollo
 */
 
+int *pedir_valores(int &var1, int &var2)
+{
+    int *vec = new int[10];
+    if(!vec)
+    {
+        cout << "Error: memoria insuficiente" << endl;
+        return vec;
+    }
+
+    int j = 23;
+    for(int i = 0; i < 10; i++)
+    {
+        vec[i] = j;
+        j++;
+    }
+
+    cout << "Ingrese un numero positivo: ";
+    cin >> var1;
+    cout << "\n";
+
+    cout << "Ingrese otro numero: ";
+    cin >> var2;
+    cout << "\n";
+
+    return vec;
+}
+
+void mostrar_valores(int *vec, int var1, int var2)
+{
+    cout << "El primer valor es: " << vec[var1] << " y el segundo es: " << vec[var2] << endl;
+    cout << "Si tenias dudas, su direccion es: " << vec << endl;
+
+    delete[] vec;
+}
+
+
 int main()
 {
     int val1 = 0;
     int val2 = 0;
 
-    cout << "Ingrese un numero positivo: ";
-    cin >> val1;
-    cout << "\n";
+    int *vec = nullptr;
 
-    cout << "Ingrese otro numero: ";
-    cin >> val2;
-    cout << "\n";
-
-    cout << "Has ingresado el numero " << val1 << " y el numero" << val2 << endl;
+    vec = pedir_valores(val1, val2);
+    mostrar_valores(vec, val1, val2);
 }
