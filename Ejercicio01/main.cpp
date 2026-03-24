ec18fcabf16ab3c6ed00e480f2c041d2efd337af
Tomas Vecchi <204607402+tvecchiUTN@users.noreply.github.com>
2026-03-23 21:48:49 -0300

    Ejercicio A completado

--- Ejercicio01/main.cpp
+++ Ejercicio01/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 
+//Este es opcional
+using namespace std;
 /*
     Ejercicio 1: Herramientas de nuestro nuevo entorno de desarrollo
 */
 
 int main()
 {
+    int val1 = 0;
+    int val2 = 0;
 
+    cout << "Ingrese un numero positivo: ";
+    cin >> val1;
+    cout << "\n";
+
+    cout << "Ingrese otro numero: ";
+    cin >> val2;
+    cout << "\n";
+
+    cout << "Has ingresado el numero " << val1 << " y el numero" << val2 << endl;
 }
