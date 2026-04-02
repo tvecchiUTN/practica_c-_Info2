7bbc124c8a8a1b49228831cb410b2ae3aedd58b6
Tomas Vecchi <204607402+tvecchiUTN@users.noreply.github.com>
2026-04-02 00:54:50 -0300

    Ejercicio 12 iniciado
    Aqui se debe hacer la clase miString y todo lo relacionado

---
+++ Ejercicio12-string/mistring.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <cstring>
+#include "mistring.h"
+
+miString::miString() {}
---
+++ Ejercicio12-string/mistring.h
@@ -0,0 +1,10 @@
+#ifndef MISTRING_H
+#define MISTRING_H
+
+class miString
+{
+public:
+    miString();
+};
+
+#endif // MISTRING_H
